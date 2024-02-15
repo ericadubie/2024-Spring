0f85ab758c14492bafbe4daf5ddd8902d4f1250b
ericadubie <ericadubie14@gmail.com>
2024-02-15 15:19:36 -0500

    package.json updates

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 console.log('Hello World')
+const express = require('express');
+
+const app=express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!')
+});
+
+app.listen(PORT, () => {
+    console.log('App listening at http://localhost:${PORT}')
+});
+
