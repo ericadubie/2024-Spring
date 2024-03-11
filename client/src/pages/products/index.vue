48d6a3025e5e12c3e18610f4e71358d51f71ebec
ericadubie <ericadubie14@gmail.com>
2024-03-11 14:58:31 -0400

    Created Flyout Component

--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
     import { ref, computed } from 'vue'
     import { type Product, getProducts } from '@/model/products'
+    import ProductCard from "@/components/ProductCard.vue"
+    import FlyOut from "@/components/FlyOut.vue"
 
     const products = ref([] as Product[])
 
@@ -28,22 +30,12 @@
 
 <template>
     <div class="product-list">
-        <div class="card" v-for="product in products" :key="product.id">
-            <div class="card-image">
-                <img :src="product.thumbnail" :alt="product.title"/>
-            </div>
+        <ProductCard v-for="product in products" :key="product.id" 
+        :product="product" @addToCart="addToCart" />
 
-            <div class="card-content">
-                <p class="price"> ${{ product.price }}</p>
-                <h3> {{ product.title }}</h3>
-                <i> {{ product.brand }}</i>
-                <p> {{ product.description }}</p>
-                <button @click="addToCart(product)" class="button is-primary"> Add to Cart</button>
-            </div>
-        </div> 
     </div>
 
-    <div class="flyout">
+    <FlyOut>
         <h1 class="title">
             The Cart
         </h1>
@@ -55,7 +47,7 @@
         </u1>
         <!-- {{ cart.length }} items totaling ${{ cart.reduce((total, item) => total + item.product.price * item.quantity, 0) }} -->
         {{ cart.length }} items totaling ${{ total }}
-    </div>
+    </FlyOut>
 </template>
 
 <style scoped>
@@ -63,54 +55,6 @@
         display: flex;
         flex-wrap: wrap;
         justify-content: space-around;
-    }
-
-    .card{
-        flex-basis: 15rem;
-        margin: 0.5rem;
-        flex-grow: 1;
-    }
-
-    h3{
-        font-weight: bold;
-    }
-
-    .price{
-        font-weight: bold;
-        font-size: xx-large;
-        color: hotpink;
-        float: right;
-    }
-
-    .flyout{
-        position: fixed;
-        top: 0;
-        right: 0;
-        width: 20rem;
-        height: 100%;
-        background-color: white;
-        border: 1px solid hotpink;
-        z-index: 100;
-        box-shadow: -1px 0 5px 0 rgba(0,0,0,.5);
-        transform: translateX(80%);
-        transition: transform 1s;
-        padding: 1rem;
-    }
-
-    .flyout.open, .flyout:hover{
-        transform: translateX(0);
-    }
-
-    .cart li {
-        display: flex;
-        align-items: center;
-        margin: 1rem 0;
-    }
-
-    .cart img {
-        width: 3rem;
-        height: 3rem;
-        margin-right: 1rem;
     }
 
 </style>
