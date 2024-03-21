f08c81dcff15f2409dffbc4a7bdc586a72c93d8c
ericadubie <ericadubie14@gmail.com>
2024-03-21 12:39:52 -0400

    Starting Express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-console.log('Hello World')
 const express = require('express');
+const users = require('./controllers/users');
 
 const app=express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello Express!')
-});
+})
+.use('/users', users)
 
 app.listen(PORT, () => {
-    console.log('App listening at http://localhost:${PORT}')
+    console.log(`App listening at http://localhost:${PORT}`)
 });
 
