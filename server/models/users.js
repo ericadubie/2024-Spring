8fc614f4ee260fd41de22b3c4b1de32f191b2576
ericadubie <ericadubie14@gmail.com>
2024-04-12 15:20:25 -0400

    built own database

--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,35 @@
+const fs = require("fs");
+
+const fileName = __dirname + "/../data/users.json";
+
 /** @type { { items: User[] } } */
-const data = require("../data/users.json");
+let data; //= require('../data/users.json');
+
+fs.access(fileName, fs.constants.F_OK, (err) => {
+  if (!err) {
+    fs.readFile(fileName, "utf8", (err, content) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      data = JSON.parse(content);
+    });
+  }
+});
+
+function save(callback) {
+  fs.writeFile(fileName, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      if (callback) {
+        callback(err);
+      }
+    }
+    if (callback) {
+      callback();
+    }
+  });
+}
 
 /**
  * @typedef {import('../../client/src/model/users').User} User
@@ -43,6 +73,7 @@ function search(q) {
 function add(user) {
   user.id = data.items.length + 1;
   data.items.push(user);
+  save();
   return user;
 }
 
@@ -57,6 +88,7 @@ function update(user) {
       ...data.items[index],
       ...user,
     };
+    save();
     return user;
   }
   return null;
@@ -70,6 +102,7 @@ function remove(id) {
   const index = data.items.findIndex((item) => item.id == id);
   if (index >= 0) {
     const deleted = data.items.splice(index, 1);
+    save();
     return deleted[0];
   }
   return null;
